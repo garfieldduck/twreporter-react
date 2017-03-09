eaa08c66f7b782cd966217a16a87c3ce8417d2ed
Mika <garfieldduck@gmail.com>
2017-03-09 16:19:36 +0800

    Update packages for webpack 2

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,27 +1,53 @@
-require('babel-polyfill');
+require('babel-polyfill')
 
 // Webpack config for development
-var autoprefixer = require('autoprefixer');
-var fs = require('fs');
-var path = require('path');
-var webpack = require('webpack');
-var WebpackIsomorphicTools = require('webpack-isomorphic-tools');
-var assetsPath = path.resolve(__dirname, '../static/dist');
-var host = (process.env.HOST || 'localhost');
-var port = parseInt(process.env.PORT) + 1 || 3001;
+var autoprefixer = require('autoprefixer')
+var fs = require('fs')
+var path = require('path')
+var webpack = require('webpack')
+var helpers = require('./helpers')
+var WebpackIsomorphicTools = require('webpack-isomorphic-tools')
+var assetsPath = path.resolve(__dirname, '../static/dist')
+var host = (process.env.HOST || 'localhost')
+var port = parseInt(process.env.PORT) + 1 || 3001
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 // https://github.com/halt-hammerzeit/webpack-isomorphic-tools
-var WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
-var webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(require('./webpack-isomorphic-tools'));
+var WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin')
+var webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(require('./webpack-isomorphic-tools'))
 
-module.exports = {
+
+var babelrc = fs.readFileSync('./.babelrc')
+var babelrcObject = {}
+
+try {
+  babelrcObject = JSON.parse(babelrc)
+} catch (err) {
+  console.error('==>     ERROR: Error parsing your .babelrc.')
+  console.error(err)
+}
+
+var babelrcObjectDevelopment = babelrcObject.env && babelrcObject.env.development || {}
+
+// merge global and dev-only plugins
+var combinedPlugins = babelrcObject.plugins || []
+combinedPlugins = combinedPlugins.concat(babelrcObjectDevelopment.plugins)
+
+var babelLoaderQuery = Object.assign({}, babelrcObject, babelrcObjectDevelopment, { plugins: combinedPlugins })
+delete babelLoaderQuery.env
+
+babelLoaderQuery.presets = babelLoaderQuery.presets.map(function (v) {
+  return v === 'es2015' ? [ 'es2015', { modules: false } ] : v
+})
+
+var webpackConfig = module.exports = {
   devtool: 'inline-source-map',
   context: path.resolve(__dirname, '..'),
   entry: {
     'main': [
       'webpack-hot-middleware/client?path=http://' + host + ':' + port + '/__webpack_hmr',
-      'bootstrap-sass!./src/themes/bootstrap.config.js',
+      'react-hot-loader/patch',
+      'bootstrap-loader',
       './src/index.js'
     ]
   },
@@ -31,44 +57,108 @@ module.exports = {
     chunkFilename: '[name]-[chunkhash].js',
     publicPath: 'http://' + host + ':' + port + '/dist/'
   },
+  performance: {
+    hints: false
+  },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loaders: [ 'babel-loader' ]
+        loader: 'happypack/loader?id=jsx',
+        include: [ path.resolve(__dirname, '../src') ]
       },
-      { test:/\.json$/,
-        loader: 'json-loader'
+      {
+        test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10240,
+          mimetype: 'application/font-woff'
+        }
       },
-      { test: /\.scss$/,
-        loaders: [
-          'style',
-          'css?modules&importLoaders=2&sourceMap&localIdentName=[name]__[local]___[hash:base64:5]' +
-          '!postcss' +
-          '!sass'
-        ]
+      // { test: /\.scss$/,
+      //   use: [
+      //     {
+      //       loader: 'style-loader'
+      //     },
+      //     {
+      //       loader: 'css-loader',
+      //       options: {
+      //         modules: true,
+      //         importLoaders: 2,
+      //         sourceMap: true,
+      //         localIdentName: '[name]__[local]___[hash:base64:5]'
+      //       }
+      //     },
+      //     {
+      //       loader: 'postcss-loader'
+      //     },
+      //     {
+      //       loader: 'sass-loader'
+      //     }
+      //   ]
+      // },
+      {
+        test: /\.scss$/,
+        loader: 'happypack/loader?id=sass',
+        include: [ path.resolve(__dirname, '../src') ]
       },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', "css-loader")
+        use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
       },
-      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?mimetype=application/vnd.ms-fontobject" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
-      { test: webpackIsomorphicToolsPlugin.regular_expression('images'), loader: 'url-loader?limit=10240' }
+      {
+        test: /\.woff?(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10240,
+          mimetype: 'application/font-woff'
+        }
+      },
+      {
+        test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10240,
+          mimetype: 'application/font-woff'
+        }
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10240,
+          mimetype: 'application/octet-stream'
+        }
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10240,
+          mimetype: 'image/svg+xml'
+        }
+      }, 
+      {
+        test: webpackIsomorphicToolsPlugin.regular_expression('images'),
+        loader: 'url-loader',
+        options: {
+          limit: 10240
+        }
+      }
     ]
   },
-  postcss: [autoprefixer],
+  postcss: [ autoprefixer ],
   progress: true,
   resolve: {
-    modulesDirectories: [
+    modules: [
       'src',
       'node_modules'
     ],
-    extensions: ['', '.json', '.js', '.jsx'],
+    extensions: [ '.json', '.js', '.jsx' ],
     alias: {
       ScrollMagic: 'scrollmagic/scrollmagic/uncompressed/ScrollMagic.js'
     }
@@ -89,6 +179,46 @@ module.exports = {
     }),
     // css files from the extract-text-plugin loader
     new ExtractTextPlugin('[name].css'),
-    webpackIsomorphicToolsPlugin.development()
+    webpackIsomorphicToolsPlugin.development(),
+
+    helpers.createHappyPlugin('jsx', [
+      {
+        loader: 'react-hot-loader/webpack'
+      }, {
+        loader: 'babel-loader',
+        query: babelLoaderQuery
+      }, {
+        loader: 'eslint-loader'
+      }
+    ]),
+    helpers.createHappyPlugin('sass', [
+      {
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        query: {
+          modules: true,
+          importLoaders: 3,
+          sourceMap: true,
+          localIdentName: '[local]___[hash:base64:5]'
+        }
+      }, {
+        loader: 'autoprefixer-loader',
+        query: {
+          browsers: 'last 2 version'
+        }
+      }, {
+        loader: 'resolve-url-loader'
+      }, {
+        loader: 'sass-loader',
+        query: {
+          outputStyle: 'expanded',
+          sourceMap: true
+        }
+      },
+      {
+        loader: 'postcss-loader'
+      }
+    ])
   ]
-};
+}
--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,19 +1,19 @@
-require('babel-polyfill');
+require('babel-polyfill')
 
 // Webpack config for creating the production bundle.
-var autoprefixer = require('autoprefixer');
-var path = require('path');
-var webpack = require('webpack');
-var CleanPlugin = require('clean-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var strip = require('strip-loader');
+var autoprefixer = require('autoprefixer')
+var path = require('path')
+var webpack = require('webpack')
+var CleanPlugin = require('clean-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var strip = require('strip-loader')
 
-var projectRootPath = path.resolve(__dirname, '../');
-var assetsPath = path.resolve(projectRootPath, './static/dist');
+var projectRootPath = path.resolve(__dirname, '../')
+var assetsPath = path.resolve(projectRootPath, './static/dist')
 
 // https://github.com/halt-hammerzeit/webpack-isomorphic-tools
-var WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
-var webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(require('./webpack-isomorphic-tools'));
+var WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin')
+var webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(require('./webpack-isomorphic-tools'))
 
 module.exports = {
   devtool: 'source-map',
@@ -31,47 +31,55 @@ module.exports = {
     publicPath: '/dist/'
   },
   module: {
-    loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loaders: [strip.loader('debug'), 'babel']},
+    rules: [
+      { test: /\.jsx?$/, exclude: /node_modules/, 
+        use: [
+          {
+            loader: strip.loader('debug')
+          },
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
       { test: /node_modules\/react-flex-carousel\/index.js/, loader: 'babel-loader' },
-      { test: /\.json$/, loader: 'json-loader' },
       { test: /\.scss$/,
-        loader: ExtractTextPlugin.extract(
-          'style',
-          'css?modules&importLoaders=2&sourceMap&localIdentName=[name]__[local]___[hash:base64:5]' +
-          '!postcss' +
-          '!sass'
-        )
+        use: ExtractTextPlugin.extract({ 
+          fallback: 'style-loader', 
+          use: 'css-loader?modules&importLoaders=2&sourceMap&localIdentName=[name]__[local]___[hash:base64:5]' +
+          '!postcss-loader' +
+          '!sass-loader'
+        })
       },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style-loader', "css-loader")
+        use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
       },
-      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' },
       { test: webpackIsomorphicToolsPlugin.regular_expression('images'), loader: 'url-loader?limit=10240' },
     ]
   },
-  postcss: [autoprefixer],
+  postcss: [ autoprefixer ],
   progress: true,
   resolve: {
-    modulesDirectories: [
+    modules: [
       'src',
       'node_modules'
     ],
-    extensions: ['', '.json', '.js', '.jsx'],
+    extensions: [ '', '.json', '.js', '.jsx' ],
     alias: {
       ScrollMagic: 'scrollmagic/scrollmagic/uncompressed/ScrollMagic.js'
     }
   },
   plugins: [
-    new CleanPlugin([assetsPath], { root: projectRootPath }),
+    new CleanPlugin([ assetsPath ], { root: projectRootPath }),
 
     // css files from the extract-text-plugin loader
-    new ExtractTextPlugin('[name]-[chunkhash].css', {allChunks: true}),
+    new ExtractTextPlugin('[name]-[chunkhash].css', { allChunks: true }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"',
@@ -87,7 +95,9 @@ module.exports = {
     new webpack.IgnorePlugin(/\.\/dev/, /\/config$/),
 
     // optimizations
-    new webpack.optimize.DedupePlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    }),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
@@ -97,4 +107,4 @@ module.exports = {
 
     webpackIsomorphicToolsPlugin
   ]
-};
+}
